128bfdb089a5a30fe8fa1c2291e39a9f8bd0cfa8
Deven Phillips <deven.phillips@redhat.com>
2020-08-06 12:11:18 -0400

    Update Vert.x example to support native image

--- samples/vertx/src/main/java/com/example/helloworld/HelloWorld.java
+++ samples/vertx/src/main/java/com/example/helloworld/HelloWorld.java
@@ -1,11 +1,25 @@
 package com.example.helloworld;
 
 import io.reactivex.Flowable;
+import io.vertx.core.VertxOptions;
+import io.vertx.core.eventbus.EventBusOptions;
 import io.vertx.reactivex.core.AbstractVerticle;
+import io.vertx.reactivex.core.Vertx;
 import io.vertx.reactivex.core.http.HttpServer;
 import io.vertx.reactivex.core.http.HttpServerRequest;
 
 public class HelloWorld extends AbstractVerticle {
+
+    public static void main(String[] args) {
+        EventBusOptions evOpts = new EventBusOptions()
+            .setClustered(false);
+        VertxOptions opts = new VertxOptions();
+        opts.setPreferNativeTransport(false);
+        opts.setHAEnabled(false);
+        opts.setEventBusOptions(evOpts);
+            
+        Vertx.vertx(opts).deployVerticle(new HelloWorld());
+    }
 
     public void start() {
 
